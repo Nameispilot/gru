c156d283c9db237ee3ac5009645797b41a6d7f13
Nameispilot <nameispilot69@gmai.com>
2021-10-29 14:34:56 +0300

    added forward + cost

--- network.go
+++ network.go
@@ -18,6 +18,8 @@ type network struct {
 
 	//previous node simulation
 	prev *gorgonia.Node
+
+	cost *gorgonia.Node
 }
 
 func MakeNetwork(g *gorgonia.ExprGraph, inputSize, embeddingSize, outputSize, hiddenSize int) *network {
@@ -47,12 +49,12 @@ type GRUOut struct {
 	hidden, prob *gorgonia.Node
 }
 
-func (n *network) Fwd(sourceIdx int, prev *gorgonia.Node) (*GRUOut, error) {
+func (n *network) fwd(sourceIdx int, prev *GRUOut) (*GRUOut, error) {
 	var prevState *gorgonia.Node
 	if prev == nil {
 		prevState = n.prev
 	} else {
-		prevState = prev
+		prevState = prev.hidden
 	}
 
 	var inputVector, hidden *gorgonia.Node
@@ -81,4 +83,69 @@ func (n *network) Fwd(sourceIdx int, prev *gorgonia.Node) (*GRUOut, error) {
 	}
 	return retVal, nil
 
+}
+
+func (n *network) Fwd(input *gorgonia.Node) error {
+	var sentence []int
+	var err error
+	if input.Type().String() == "Vector int" {
+		sentence = input.Value().Data().([]int)
+	} else {
+		return errors.Wrap(err, "Input vector is not' 'Int'")
+	}
+
+	vocabIndex := make(map[int]int)
+	for i, v := range sentence {
+		vocabIndex[v] = i
+	}
+
+	var source, target int
+	var prev *GRUOut
+	var cost *gorgonia.Node
+	for i := 0; i < len(sentence)-1; i++ {
+		source = sentence[i]
+		target = sentence[i+1]
+		sourceId := vocabIndex[source]
+		targetId := vocabIndex[target]
+
+		if prev, err = n.fwd(sourceId, prev); err != nil {
+			return errors.Wrap(err, "Can't feed forward")
+		}
+
+		if cost, err = costLSTM(prev.prob, cost, targetId); err != nil {
+			return errors.Wrap(err, "Can't calculate cost")
+		}
+	}
+
+	n.cost = cost
+	return nil
+}
+
+func costLSTM(prob, cost *gorgonia.Node, targetId int) (*gorgonia.Node, error) {
+	logprob, err := gorgonia.Log(prob)
+	if err != nil {
+		return nil, errors.Wrap(err, "...")
+	}
+	logprob, err = gorgonia.Neg(logprob)
+	if err != nil {
+		return nil, errors.Wrap(err, "...")
+	}
+	loss, err := gorgonia.Slice(logprob, gorgonia.S(targetId))
+	if err != nil {
+		return nil, errors.Wrap(err, "...")
+	}
+	if cost == nil {
+		cost = loss
+	} else {
+		cost, err = gorgonia.Add(cost, loss)
+		if err != nil {
+			return nil, errors.Wrap(err, "...")
+		}
+	}
+
+	return cost, nil
+}
+
+func (n *network) GetCost() *gorgonia.Node {
+	return n.cost
 }
